ca49d29fea2f1903b593f658bd3f4670613036da
AjaypalSinghGill <ajaypal.gill1@oyorooms.com>
2022-03-16 21:37:20 +0530

    api appraoch 2

--- webhookApi.py
+++ webhookApi.py
@@ -1,6 +1,32 @@
 import requests
-
+from tabulate import tabulate
 response = requests.get('https://kubecost.k8s2.oyorooms.io/model/allocation?window=24h&aggregate=deployment&external=true&accumulate=true&shareIdle=false&idleByNode=false&filterNamespaces=owndel')
-print(response.json()["data"])
+# print(response.json()["data"][0])
+
+
+costMatrix = []
+
+tableHeader = ["Name", "CPU", "GPU", "RAM", "Efficiency"]
+costMatrix.append(tableHeader)
+
+ 
+for key, value in response.json()["data"][0].iteritems():
+	tableRow = []
+	tableRow.append(str(key))
+	tableRow.append("$" + str(round(value['cpuCost'], 2)))
+	tableRow.append("$" + str(round(value['gpuCost'], 2)))
+	tableRow.append("$" + str(round(value['ramCost'], 2)))
+	tableRow.append("$" + str(round(value['totalEfficiency'],2)))
+	costMatrix.append(tableRow)
+	# print(key)
+	# print(value)
+# print(costMatrix)
+print(tabulate(costMatrix, tablefmt='html'))
+# for x in response.json()["data"]:
+
+  
+
+
+
 
 
