99af0855e4e61b82e05710be70868d0bf345da95
AjaypalSinghGill <ajaypal.gill1@oyorooms.com>
2022-03-16 22:39:12 +0530

    api appraoch 3

--- webhookApi.py
+++ webhookApi.py
@@ -1,5 +1,40 @@
 import requests
 from tabulate import tabulate
+from datetime import date, timedelta
+
+
+def getTotalCostOfNthDay(n):
+	d = date.today() - timedelta(days=n)
+	startDate = d
+	endDate = date.today() - timedelta(days=(n-1))
+	print(startDate)
+	print(endDate)
+	url = 'https://kubecost.k8s2.oyorooms.io/model/allocation?window={startDate}T00%3A00%3A00Z%2C{endDate}T00%3A00%3A00Z&aggregate=deployment&external=true&accumulate=true&shareIdle=false&idleByNode=false&shareTenancyCosts=true&filterNamespaces=owndel'.format(startDate = startDate, endDate = endDate)
+	response = requests.get(url)
+
+	costMatrix = []
+
+	tableHeader = ["Name", "CPU", "GPU", "RAM", "Efficiency"]
+	costMatrix.append(tableHeader)
+
+	totalCpuCost = 0
+	totalGpuCost = 0
+	totalRamCost = 0
+	 
+	for key, value in response.json()["data"][0].iteritems():
+		totalCpuCost = totalCpuCost + round(value['cpuCost'], 2)
+		totalGpuCost = totalGpuCost + round(value['gpuCost'], 2)
+		totalRamCost = totalRamCost + round(value['ramCost'], 2)
+
+	totalCostRow = ["Total Cost({date})".format(date=startDate),"$" + str(totalCpuCost),"$" +  str(totalGpuCost),"$" +  str(totalRamCost)]
+
+	return totalCostRow
+
+
+
+
+
+
 response = requests.get('https://kubecost.k8s2.oyorooms.io/model/allocation?window=24h&aggregate=deployment&external=true&accumulate=true&shareIdle=false&idleByNode=false&filterNamespaces=owndel')
 # print(response.json()["data"][0])
 
@@ -9,20 +44,58 @@ costMatrix = []
 tableHeader = ["Name", "CPU", "GPU", "RAM", "Efficiency"]
 costMatrix.append(tableHeader)
 
+totalCpuCost = 0
+totalGpuCost = 0
+totalRamCost = 0
  
 for key, value in response.json()["data"][0].iteritems():
+	totalCpuCost = totalCpuCost + round(value['cpuCost'], 2)
+	totalGpuCost = totalGpuCost + round(value['gpuCost'], 2)
+	totalRamCost = totalRamCost + round(value['ramCost'], 2)
 	tableRow = []
+	if(str(key)=="__idle__"):
+		continue
 	tableRow.append(str(key))
 	tableRow.append("$" + str(round(value['cpuCost'], 2)))
 	tableRow.append("$" + str(round(value['gpuCost'], 2)))
 	tableRow.append("$" + str(round(value['ramCost'], 2)))
-	tableRow.append("$" + str(round(value['totalEfficiency'],2)))
+	tableRow.append(str(round(value['totalEfficiency'],2)))
 	costMatrix.append(tableRow)
 	# print(key)
 	# print(value)
 # print(costMatrix)
+
+totalCostRow = ["Total Cost(Last 24 hours)","$" + str(totalCpuCost),"$" +  str(totalGpuCost),"$" +  str(totalRamCost)]
+
+costMatrix.append(totalCostRow)
+# finalCostMatrix = []
+
+# finalCostMatrix.append(totalCostRow)
+
+# finalCostMatrix.update(costMatrix)
+costMatrix.append(getTotalCostOfNthDay(2))
+costMatrix.append(getTotalCostOfNthDay(3))
+costMatrix.append(getTotalCostOfNthDay(9))
 print(tabulate(costMatrix, tablefmt='html'))
 # for x in response.json()["data"]:
+
+webhook_url="https://oyoenterprise.webhook.office.com/webhookb2/ad40733b-2c8e-4d8a-833c-016e5d1d3030@04ec3963-dddc-45fb-afb7-85fa38e19b99/IncomingWebhook/65e3f90f975242ef87d4a6c28cca44d3/a3fa0b73-93cc-4cf0-945e-049bd89348e7"
+
+
+responseWebhook = requests.post(
+        url=webhook_url,
+        headers={"Content-Type": "application/json"},
+        json={
+            "themeColor": "000000",
+            "summary": "Cost metrics of all service",
+            "sections": [{
+                "activityTitle": "Cost metrics of all service for past 24 hours",
+                "activitySubtitle": tabulate(costMatrix, tablefmt='html')
+            }],
+        },
+    )
+
+
 
   
 
