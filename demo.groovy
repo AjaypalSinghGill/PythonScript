28618e24e06ad8478b41b4a918379db3044fad0a
AjaypalSinghGill <ajaypal.gill1@oyorooms.com>
2022-04-06 19:49:05 +0530

    s

---
+++ demo.groovy
@@ -0,0 +1,17 @@
+
+pipeline {
+    stages {
+        stage("Image build and push in China") {
+            steps {
+                script {
+                    start {"node --max-old-space-size=8192"}
+                    sh """
+                    node -e 'console.log(v8.getHeapStatistics().heap_size_limit/(1024*1024))'
+                    export NODE_OPTIONS="--max-old-space-size=8192"
+                    echo $NODE_OPTIONS
+                    """
+                }
+            }
+        }
+    }
+}
