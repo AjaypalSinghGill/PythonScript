0337d4effa8ba69675c96812b3a5bb2792b996dd
AjaypalSinghGill <ajaypal.gill1@oyorooms.com>
2022-04-06 20:03:04 +0530

    s

--- demo.groovy
+++ demo.groovy
@@ -7,10 +7,10 @@ pipeline {
                 script {
                     sh """
                     node -e 'console.log(v8.getHeapStatistics().heap_size_limit/(1024*1024))'
-                    export NODE_OPTIONS="--max-old-space-size=8192"
-                    node -e 'console.log(v8.getHeapStatistics().heap_size_limit/(1024*1024))'
-                    node --max-old-space-size=8192
-                    echo $NODE_OPTIONS
+//                    export NODE_OPTIONS="--max-old-space-size=8192"
+//                    node -e 'console.log(v8.getHeapStatistics().heap_size_limit/(1024*1024))'
+//                    node --max-old-space-size=8192
+//                    echo $NODE_OPTIONS
                     """
                 }
             }
